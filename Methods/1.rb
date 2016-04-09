7a48e3ebbdf914812069bd907dd94f21ce7842a5
Daniel Crittenden <dan.crittenden@gmail.com>
2016-04-09 14:21:31 -0400

    Uploading 'Methods' excercises, the third section of the workbook

---
+++ Methods/1.rb
@@ -0,0 +1,7 @@
+def greeting(name)
+  puts "Hello #{name}!"
+end
+
+puts "What is your name?"
+name = gets.chomp
+greeting(name)
---
+++ Methods/5.rb
@@ -0,0 +1,8 @@
+def scream(words)
+  words = words + "!!!!"
+  # Removed return line
+  puts words
+end
+
+scream("Yippeee")
+puts "Self explanatory."
